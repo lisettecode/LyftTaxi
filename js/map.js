0e331c02574b942bc465f2ae9ff34d5eb06cf32f
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2017-02-16 11:53:39 -0500

    Actualización Google Map

--- js/map.js
+++ js/map.js
@@ -1,37 +1,321 @@
-var map;
-function initMap() {
-  var mapConfig = {
-      center: {
-          lat: -16.45738919999998,
-          lng:- 71.53153038,
-      },
-      zoom:14
-  });
-    
-    
-miMapa = new google.maps.Map(document.getElementById('map'), mapConfig);
-    
-    var latLongPazPeru = {lat: -16.45738919999998, lng:-71.53153038};
-    var latLongPazPeru = {lat: -16.457489199999398, lng:-71.5245338};
-    var latLongPazPeru = {lat: -16.457289199999398, lng:-71.5285338};
-    var latLongPazPeru = {lat: -16.457989199999398, lng:-71.5215338};
-    var latLongPazPeru = {lat: -16.457789199999398, lng:-71.5295338};
+function init () {
+	
 }
+//para definir lat y lng
 
+var miMap;
 
-
-
-
-
-
-
-
-
-
-/*var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: -16.45738919999998, lng: -71.5315308},
+    zoom: 14
   });
-}*/
+
+//para marcadores
+
+var myLatLng = {lat: -16.45738919999998, lng: -71.5315308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/womanUser.png"
+    });
+    
+var myLatLng4 = {lat: -16.41438919999998, lng: -71.2645308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}    
+    
+var myLatLng3 = {lat: -16.45238939999998, lng: -71.5795308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+    
+var myLatLng4 = {lat: -16.43748919999998, lng: -71.5727508};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+
+var myLatLng = {lat: -16.45568919999998, lng: -71.5365308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/boyUser.jpg"
+    });
+    
+var myLatLng3 = {lat: -16.45998939999998, lng: -71.5125308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });    
+
+var myLatLng4 = {lat: -16.44274519999998, lng: -71.3745308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+    
+var myLatLng = {lat: -16.45938919999998, lng: -71.5715308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/boyUser.jpg"
+    });
+
+var myLatLng4 = {lat: -16.44374519999998, lng: -71.5274308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+
+var myLatLng = {lat: -16.45938919999998, lng: -71.5615308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/womanUser.png"
+    });
+    
+var myLatLng3 = {lat: -16.479119999998, lng: -71.5467308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });    
+    
+var myLatLng4 = {lat: -16.44933729999998, lng: -71.5737408};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+    
+var myLatLng = {lat: -16.47568919999998, lng: -71.5465308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/boyUser.jpg"
+    });    
+
+var myLatLng3 = {lat: -16.471219999998, lng: -71.5107308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+
+var myLatLng4 = {lat: -16.43745919999998, lng: -71.2745308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+    
+var myLatLng3 = {lat: -16.479156999998, lng: -71.5467308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+    
+var myLatLng2 = {lat: -16.46134919979998, lng: -71.5394308};
+var cerca1 = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'CarroPresidencial',
+    icon: "img/descarga.png"
+    });
+
+var myLatLng4 = {lat: -16.44364219999998, lng: -71.5375308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+    
+var myLatLng = {lat: -16.47868919999998, lng: -71.5365308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/boyUser.jpg"
+    });  
+    
+var myLatLng2 = {lat: -16.43134919979998, lng: -71.6394308};
+var cerca1 = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'CarroPresidencial',
+    icon: "img/descarga.png"
+    });    
+
+var myLatLng4 = {lat: -16.44683519999998, lng: -71.5846308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+
+var myLatLng3 = {lat: -16.449119999998, lng: -71.5337308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+    
+var myLatLng = {lat: -16.43638919999998, lng: -71.5515308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/womanUser.png"
+    });
+
+var myLatLng4 = {lat: -16.44390519999998, lng: -71.9635308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+    
+var myLatLng2 = {lat: -16.44734919979998, lng: -71.5484308};
+var cerca1 = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'CarroPresidencial',
+    icon: "img/descarga.png"
+    });    
+    
+var myLatLng = {lat: -16.46768919999998, lng: -71.5225308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/boyUser.jpg"
+    });   
+    
+var myLatLng2 = {lat: -16.43644919979998, lng: -71.6754308};
+var cerca1 = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'CarroPresidencial',
+    icon: "img/descarga.png"
+    });    
+    
+var myLatLng4 = {lat: -16.44939831999998, lng: -71.5735308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+var myLatLng3 = {lat: -16.45538939999998, lng: -71.5455308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+    
+var myLatLng2 = {lat: -16.46365919979998, lng: -71.6344308};
+var cerca1 = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'CarroPresidencial',
+    icon: "img/descarga.png"
+    });    
+    
+var myLatLng4 = {lat: -16.48338919999998, lng: -71.5783648};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+var myLatLng = {lat: -16.48798919999998, lng: -71.5385308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/womanUser.png"
+    });
+    
+var myLatLng4 = {lat: -16.44938919999998, lng: -71.5715308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+
+var myLatLng4 = {lat: -16.48374519999998, lng: -71.58375308};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
+var myLatLng3 = {lat: -16.412119999998, lng: -71.5367308};
+var cerca2 = new google.maps.Marker({
+    position: myLatLng3,
+    map: map,
+    title: 'CarroVip',
+    icon: "img/auto1.jpg"
+    });
+
+var myLatLng = {lat: -16.41798919999998, lng: -71.5715308};
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: '¡Aquí estoy!',
+    icon: "img/womanUser.png"
+    });
+
+var myLatLng4 = {lat: -16.44384919999998, lng: -71.5794728};
+var cerca3 = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'CarroSorpresa',
+    icon: "img/carro.png"
+    });
+}
